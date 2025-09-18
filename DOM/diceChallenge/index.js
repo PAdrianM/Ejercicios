359db914bcf5cf47baa07f3d077b5c74e9a7ca22
Adrian Garcia <agarcia@digitalgeko.com>
2025-09-18 12:29:28 -0600

    Exercise Dice, to test and practice the DOM.

---
+++ DOM/diceChallenge/index.js
@@ -0,0 +1,21 @@
+
+var randomNumber1;
+var randomNumber2;
+
+randomNumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber1);
+randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2);
+
+document.querySelector(".img1").src = `images/dice${randomNumber1}.png`;
+document.querySelector(".img2").src = `images/dice${randomNumber2}.png`;
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent = "Player 1 Wins 🚩"
+} else if ( randomNumber2 > randomNumber1) {
+    document.querySelector("h1").textContent = "Player 2 Wins 🚩"
+} else if ( randomNumber1 === randomNumber2) {
+    document.querySelector("h1").textContent = "Draw!!!"
+} else {
+    document.querySelector("h1").textContent = "Refresh Me"
+}
